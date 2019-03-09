b0a78af274b2be0823999fedf7acba4a3ce43752
Kumar <skumar85@BANL16388e3e6.local>
2019-03-10 02:30:25 +0530

    setup project obbject validation

--- ppmtool/src/main/java/com/subhbash/ppmtool/domain/Project.java
+++ ppmtool/src/main/java/com/subhbash/ppmtool/domain/Project.java
@@ -2,12 +2,17 @@ package com.subhbash.ppmtool.domain;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 public class Project {
@@ -15,10 +20,17 @@ public class Project {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@NotBlank(message = "Project name is required")
 	private String projectName;
+	@NotBlank(message = "Project identifier is required")
+	@Size(min=4, max=5, message="please use 4 to 5 characters")
+	@Column(updatable = false, unique = true)
 	private String projectIdentifier;
+	@NotBlank(message = "Project description is required")
 	private String description;
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date start_date;
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date end_date;
 	
 	private Date created_At;
