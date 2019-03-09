7e920ba13ef2923ed5be2cd8a35a442308c20233
Kumar <skumar85@BANL16388e3e6.local>
2019-03-10 02:20:02 +0530

    setup project obbject validation

---
+++ ppmtool/src/main/java/com/subhbash/ppmtool/repository/ProjectRepository.java
@@ -0,0 +1,12 @@
+package com.subhbash.ppmtool.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.subhbash.ppmtool.domain.Project;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+	
+
+}
