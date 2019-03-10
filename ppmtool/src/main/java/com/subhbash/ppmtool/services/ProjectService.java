5ffdad9eb8a7568f51ce49d786daf7ebf56553dc
Kumar <skumar85@BANL16388e3e6.connectify>
2019-03-10 15:22:47 +0530

    Custom Exception for unique projectIdentifier

--- ppmtool/src/main/java/com/subhbash/ppmtool/services/ProjectService.java
+++ ppmtool/src/main/java/com/subhbash/ppmtool/services/ProjectService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.subhbash.ppmtool.domain.Project;
+import com.subhbash.ppmtool.exceptions.ProjectIdException;
 import com.subhbash.ppmtool.repository.ProjectRepository;
 
 @Service
@@ -14,8 +15,15 @@ public class ProjectService {
 	
 	public Project saveOrUpdateProject(Project project) {
 		
-		// logic 
+		// logic
+		try {
+			project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+			return projectRepository.save(project);
+			
+		} catch (Exception e) {
+			throw new ProjectIdException("Project Id '"+project.getProjectIdentifier().toUpperCase()+"' already exists");
+		}
 		
-		return projectRepository.save(project);
+		
 	}
 }
