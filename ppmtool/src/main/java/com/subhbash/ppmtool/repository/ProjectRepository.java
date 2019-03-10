6e3ebf32dd06732ac46292d025a8efc1b65c55ff
Kumar <skumar85@BANL16388e3e6.connectify>
2019-03-10 17:24:43 +0530

    Find all project

--- ppmtool/src/main/java/com/subhbash/ppmtool/repository/ProjectRepository.java
+++ ppmtool/src/main/java/com/subhbash/ppmtool/repository/ProjectRepository.java
@@ -10,4 +10,7 @@ public interface ProjectRepository extends CrudRepository<Project, Long> {
 	
 
 	Project findByProjectIdentifier(String projectId);
+	
+	@Override
+	Iterable<Project> findAll();
 }
--- ppmtool/src/main/java/com/subhbash/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/com/subhbash/ppmtool/web/ProjectController.java
@@ -49,6 +49,11 @@ public class ProjectController {
 		return new ResponseEntity<Project>(project, HttpStatus.OK);
 	}
 	
+	@GetMapping("/all")
+	public Iterable<Project> getAllProject(){
+		return projectService.findAllProjects();
+	}
+	
 	
 	
 
