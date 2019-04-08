060a361df0422fc313005014c45bd430469476b3
Kumar <skumar85@BANL16388e3e6.connectify>
2019-04-08 23:42:20 +0530

    Added nav bar

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import ProjectItem from "./Projects/ProjectItem";
 
 class Dashboard extends Component {
   render() {
     return (
       <div>
-        <h1>Welcome to the Dashboard</h1>
+        <h1>Welcome to dashboard</h1>
+        <ProjectItem />
       </div>
     );
   }
