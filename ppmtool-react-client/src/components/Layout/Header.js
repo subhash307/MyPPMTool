2fb49274beac81d165c104492d17433ea0324755
Kumar <skumar85@BANL16388e3e6.connectify>
2019-04-14 15:49:06 +0530

    React Router and First Functional Component

--- ppmtool-react-client/src/components/Layout/Header.js
+++ ppmtool-react-client/src/components/Layout/Header.js
@@ -5,7 +5,7 @@ class Header extends Component {
     return (
       <nav className="navbar navbar-expand-sm navbar-dark bg-primary mb-4">
         <div className="container">
-          <a className="navbar-brand" href="Dashboard.html">
+          <a className="navbar-brand" href="/">
             Personal Project Management Tool
           </a>
           <button
@@ -20,7 +20,7 @@ class Header extends Component {
           <div className="collapse navbar-collapse" id="mobile-nav">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item">
-                <a className="nav-link" href="/dashboard">
+                <a className="nav-link" href="/">
                   Dashboard
                 </a>
               </li>
---
+++ ppmtool-react-client/src/components/Projects/addProject.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class addProject extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Add Project Form</h1>
+      </div>
+    );
+  }
+}
+
+export default addProject;
