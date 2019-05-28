1742aa4f85c3d28188e0aecba7a422e3ef88b9c9
Subhash Kumar <subhash.kumar307@gmail.com>
2019-05-28 15:48:32 +0530

    Routes to project board and project tasks

--- ppmtool-react-client/src/actions/projectActions.js
+++ ppmtool-react-client/src/actions/projectActions.js
@@ -3,7 +3,7 @@ import { GET_ERRORS, GET_PROJECTS, GET_PROJECT, DELETE_PROJECT } from "./types";
 
 export const createProject = (project, history) => async dispatch => {
   try {
-    const res = await axios.post("/api/project", project);
+    await axios.post("/api/project", project);
     history.push("/dashboard");
     dispatch({
       type: GET_ERRORS,
@@ -40,10 +40,10 @@ export const getProject = (id, history) => async dispatch => {
 export const deleteProject = id => async dispatch => {
   if (
     window.confirm(
-      `Are you sure? you want to delete the project \"${id}\" and all the data realated to it.`
+      `Are you sure? you want to delete the project "${id}  " and all the data realated to it.`
     )
   ) {
-    const res = await axios.delete(`/api/project/${id}`);
+    await axios.delete(`/api/project/${id}`);
     dispatch({
       type: DELETE_PROJECT,
       payload: id
