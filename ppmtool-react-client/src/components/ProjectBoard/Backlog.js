309e754eeb666f063534b31a62af352e893ddc2e
Subhash Kumar <subhash.kumar307@gmail.com>
2019-05-29 12:01:48 +0530

    setup projectboard, Backlog, ProjectTask component

---
+++ ppmtool-react-client/src/components/ProjectBoard/Backlog.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import ProjectTask from "./ProjectTasks/ProjectTask";
+
+export class Backlog extends Component {
+  static propTypes = {
+    prop: PropTypes
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <div className="card text-center mb-2">
+              <div className="card-header bg-secondary text-white">
+                <h3>TO DO</h3>
+              </div>
+            </div>
+            <ProjectTask />
+          </div>
+          <div className="col-md-4">
+            <div className="card text-center mb-2">
+              <div className="card-header bg-primary text-white">
+                <h3>In Progress</h3>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="card text-center mb-2">
+              <div className="card-header bg-success text-white">
+                <h3>Done</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({});
+
+const mapDispatchToProps = {};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Backlog);
