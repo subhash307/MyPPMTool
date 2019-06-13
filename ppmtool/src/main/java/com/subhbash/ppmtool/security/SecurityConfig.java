30f26182569d4147e3c6bf1d30154f516f67f53d
Subhash Kumar <subhash.kumar307@gmail.com>
2019-06-13 16:23:44 +0530

    JWT Provider pre-work

--- ppmtool/src/main/java/com/subhbash/ppmtool/security/SecurityConfig.java
+++ ppmtool/src/main/java/com/subhbash/ppmtool/security/SecurityConfig.java
@@ -1,13 +1,24 @@
 package com.subhbash.ppmtool.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.subhbash.ppmtool.services.CustomUserDetailsService;
+
+import static com.subhbash.ppmtool.security.SecurityConstants.SIGN_UP_URLS;
+import static com.subhbash.ppmtool.security.SecurityConstants.H2_URL;
 
 @Configuration
 @EnableWebSecurity
@@ -20,6 +31,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Autowired
 	private JwtAuthenticationEntryPoint unauthorizedHeadr;
+
+	@Autowired
+	private CustomUserDetailsService customUserDetailsService;
+	
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+		authenticationManagerBuilder.userDetailsService(customUserDetailsService).passwordEncoder(bCryptPasswordEncoder);
+		
+	}
+
+	@Override
+	@Bean(BeanIds.AUTHENTICATION_MANAGER)
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
+
+
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -42,7 +73,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 						"/**/*.css",
 						"/**/*.js"
 						).permitAll()
-				.antMatchers("/api/users/**").permitAll()
+				.antMatchers(SIGN_UP_URLS).permitAll()
+				.antMatchers(H2_URL).permitAll()
 				.anyRequest().authenticated();
 
 		
