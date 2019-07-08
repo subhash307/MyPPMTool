ee1e94ee1dee3ed16f6cd2fa211ab64516df56df
Subhash Kumar <subhash.kumar307@gmail.com>
2019-07-08 11:14:56 +0530

    Polyfill to fix internet explorer isssue

---
+++ ppmtool/src/main/resources/static/precache-manifest.40393667c8e3bea44badda4aa069e296.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b58a3f27f62c0a07f522",
+    "url": "/static/js/main.b6135918.chunk.js"
+  },
+  {
+    "revision": "6001fdb0122b00661fd8",
+    "url": "/static/js/2.a7967a38.chunk.js"
+  },
+  {
+    "revision": "b58a3f27f62c0a07f522",
+    "url": "/static/css/main.0e25d5c0.chunk.css"
+  },
+  {
+    "revision": "6001fdb0122b00661fd8",
+    "url": "/static/css/2.04cb5f94.chunk.css"
+  },
+  {
+    "revision": "98b1200f6167e70d81ed9c01e29b1a77",
+    "url": "/index.html"
+  }
+];
